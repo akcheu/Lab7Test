a1325370dd3245d459bf1b91e6e8b6043062f816
Allen <akcheung@ucsd.edu>
2021-05-16 15:17:59 -0700

    New

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+// script.js
+
+import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
+const setState = router.setState;
+
+// Make sure you register your service worker here too
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://cse110lab6.herokuapp.com/entries')
+    .then(response => response.json())
+    .then(entries => {
+      entries.forEach(entry => {
+        let newPost = document.createElement('journal-entry');
+        newPost.entry = entry;
+        document.querySelector('main').appendChild(newPost);
+      });
+    });
+});
